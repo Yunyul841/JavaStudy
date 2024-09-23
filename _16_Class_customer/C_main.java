76ee1585dc930fe0495a582507261f2ef7182911
Yunyul <gmldnd93@gmail.com>
2024-09-23 18:53:53 +0900

    2024-09-23

--- _16_Class_customer/C_main.java
+++ _16_Class_customer/C_main.java
@@ -1,10 +1,29 @@
 package _16_Class_customer;
 
+import java.util.Scanner;
+
 public class C_main {
 
 	public static void main(String[] args) {
-//		C_center cust = new C_center();
-		E_center qwer = new E_center();
+		while(true) {
+			System.out.println("1. 고객");
+			System.out.println("2. 이벤트");
+			Scanner in = new Scanner(System.in);
+			int aaa = in.nextInt();
+			in.nextLine();
+			if(aaa == 1) {
+				C_center cust = new C_center();	
+			}else if(aaa == 2) {
+				E_center qwer = new E_center();				
+			}else {
+				System.out.println("끝");
+				break;
+			}
+			
+					
+		
+		}
+		
 		
 		}
 }
